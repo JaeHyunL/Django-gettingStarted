066f3c20c79a6c49c5766cdb1f4ad3214bf2615e
JaeHyunL <swaa23@gmail.com>
2024-07-24 23:33:55 +0900

    ⚙️build: add Unit_test

--- app/inventory/tests.py
+++ app/inventory/tests.py
@@ -1,3 +1,11 @@
 from django.test import TestCase
+from return_hello import hello
+
 
 # Create your tests here.
+class DumyTestCase(TestCase):
+    def setUp(self) -> None:
+        return super().setUp()
+
+    def test_dummy_test(self):
+        self.assertEqual("hello", hello())
